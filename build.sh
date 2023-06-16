666cb9a68752c8c409f028ed916b7bf69e76e9b0
jlmobbex <javier.lopez@mobbex.com>
2023-06-16 17:25:44 -0300

    Update version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VER="1.1.1"
+VER="1.1.2"
 
 # Unified Version
 PRESTAV="1.6-1.7"
--- mobbex_marketplace/mobbex_marketplace.php
+++ mobbex_marketplace/mobbex_marketplace.php
@@ -6,7 +6,7 @@
  * Main file of the module
  *
  * @author  Mobbex Co <admin@mobbex.com>
- * @version 1.1.1
+ * @version 1.1.2
  * @see     PaymentModuleCore
  */
 
